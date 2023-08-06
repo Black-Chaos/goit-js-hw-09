7c80fa230e0dfc33b460568a3875c15822288c09
Black Chaos <black.chaos.no1@gmail.com>
2023-08-06 20:57:20 +0300

    feat: task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+import { getRandomHexColor } from "./partials/randomfn";
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let ID = null
+
+start.addEventListener('click', startChangeColor)
+stop.addEventListener('click', stopChangeColor)
+
+function startChangeColor() {
+    ID = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000)
+    start.disabled = true;
+    stop.disabled = false;
+}
+
+function stopChangeColor() {
+    clearInterval(ID);
+    start.disabled = false;
+    stop.disabled = true;
+}
